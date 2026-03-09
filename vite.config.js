860ea99e79eeda10c1f4cd997a76015e6eac1c71
BazilSuhail <bazil1854@gmail.com>
2026-03-09 22:18:13 +0500

    feat: Started revamp of client, integrate `tailwind-css 4` configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   build: {
-    outDir: 'dist', // Explicitly define the output directory
+    outDir: 'dist',
   },
 })
